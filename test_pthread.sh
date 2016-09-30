06e8be15be262c8dfb8e4238146c4bb70b823560
Victor Saint Guilhem <vsaintguilh@enseirb-matmeca.fr>
2016-09-29 20:56:28 -0500

    test pthread correction

--- test_pthread.sh
+++ test_pthread.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
+if [ $# -ne 1 ]
+then
+    echo "Error: need to provide which solution to test (1, 2 or 3)"
+    exit
+fi
 
 program="pt"
-for i in `seq 1 3`;
-do
-    if [ -f $program$i ]
-    then
-        for matrix_size in `seq 10 10 1000`;
-        do
-            echo "Computing with $program$i for a matrix of size $matrix_size (out of 1000)"
-            ./$program$i $matrix_size 0 > /dev/null
-        done
-    else
-        echo "Error: \"$program$i\" file does't exist"
-    fi
-done
+if [ -f $program$1 ]
+then
+    for matrix_size in `seq 10 10 1000`;
+    do
+        echo "Computing with $program$1 for a matrix of size $matrix_size (out of 1000)"
+        ./$program$1 $matrix_size 0 > /dev/null
+    done
+else
+    echo "Error: \"$program$1\" file does't exist"
+fi
